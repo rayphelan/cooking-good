59c0c535399e8b54fefdac17420463c2316659ab
rayphelan <rayphelan@gmail.com>
2017-11-30 10:30:54 +0000

    Selected Recipes

---
+++ application/views/_nav-header.php
@@ -0,0 +1,51 @@
+<header id="js-header" class="u-header u-header--sticky-top u-header--change-appearance u-header--change-logo"
+              data-header-fix-moment="0">
+        <div class="u-header__section g-bg-black-opacity-0_2 g-transition-0_3 g-py-7 g-py-23--md"
+             data-header-fix-moment-exclude="g-bg-black-opacity-0_5 g-py-23--md"
+             data-header-fix-moment-classes="u-shadow-v19 g-bg-white g-py-15--md">
+          <nav class="navbar navbar-expand-lg g-py-0">
+            <div class="container g-pos-rel">
+              
+
+              <!-- Logo -->
+              <a href="#!" class="js-go-to navbar-brand u-header__logo"
+                 data-type="static">
+                <img class="u-header__logo-img u-header__logo-img--main g-width-90" src="<?php print base_url('assets/images/logo/logo.png');?>" alt="<?php print WEBSITE_TITLE;?>">
+                <img class="u-header__logo-img g-width-90" src="<?php print base_url('assets/images/logo/logo-dark.png');?>" alt="<?php print WEBSITE_TITLE;?>">
+              </a>
+              <!-- End Logo -->
+
+
+              <!-- Navigation -->
+              <div class="collapse navbar-collapse align-items-center flex-sm-row" id="navBar">
+                <ul id="js-scroll-nav" class="navbar-nav text-uppercase g-letter-spacing-1 g-font-size-12 g-pt-20 g-pt-0--lg ml-auto">
+                  <li class="nav-item g-mr-15--lg g-mb-7 g-mb-0--lg">
+                    <a href="#home-section" class="nav-link p-0">Home <span class="sr-only">(current)</span></a>
+                  </li>                  
+                  <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                    <a href="#selected-recipes" class="nav-link p-0">Selected Recipes</a>
+                  </li>                  
+                  <li class="nav-item g-mx-15--lg g-mb-7 g-mb-0--lg">
+                    <a href="#contact-section" class="nav-link p-0">Contact</a>
+                  </li>
+                  <li class="nav-item g-ml-15--lg">
+                    <a href="../../index.html" class="nav-link p-0">Main</a>
+                  </li>
+                </ul>
+              </div>
+              <!-- End Navigation -->
+
+              <!-- Responsive Toggle Button -->
+              <button class="navbar-toggler btn btn-primary g-pos-abs g-top-15 g-right-0" type="button"
+                      aria-label="Toggle navigation"
+                      aria-expanded="false"
+                      aria-controls="navBar"
+                      data-toggle="collapse"
+                      data-target="#navBar">
+                MENU
+              </button>
+              <!-- End Responsive Toggle Button -->
+            </div>
+          </nav>
+        </div>
+      </header>
---
+++ application/views/_selected-recipes.php
@@ -0,0 +1,201 @@
+<div id="selected-recipes" class="container g-py-100">
+        <!-- Heading -->
+        <div class="g-max-width-550 text-center mx-auto g-mb-70">
+          <h2 class="text-uppercase g-color-main-light-v1 g-font-weight-600 g-font-size-13 g-letter-spacing-2 mb-4">Selected Recipes</h2>
+          <h2 class="h3 mb-5">We have selected these recipes to show your first. Every week we will select and highlight new recipes.</h2>
+        </div>
+        <!-- End Heading -->
+
+        <!-- Cube Portfolio Blocks - Filter -->
+        <ul id="filterControls" class="d-block list-inline text-center g-mb-50">
+          <li class="list-inline-item cbp-filter-item cbp-filter-item-active g-brd-around g-brd-secondary-light-v2 g-brd-primary--active g-color-gray-dark-v4 g-color-primary--hover g-color-primary--active g-font-size-13 g-rounded-30 g-transition-0_3 g-px-20 g-py-7 mb-2" role="button"
+              data-filter="*">All
+          </li>
+          <li class="list-inline-item cbp-filter-item g-brd-around g-brd-secondary-light-v2 g-brd-primary--active g-color-gray-dark-v4 g-color-primary--hover g-color-primary--active g-font-size-13 g-rounded-30 g-transition-0_3 g-px-20 g-py-7 mb-2" role="button"
+              data-filter=".identity">Identity
+          </li>
+          <li class="list-inline-item cbp-filter-item g-brd-around g-brd-secondary-light-v2 g-brd-primary--active g-color-gray-dark-v4 g-color-primary--hover g-color-primary--active g-font-size-13 g-rounded-30 g-transition-0_3 g-px-20 g-py-7 mb-2" role="button"
+              data-filter=".design">Design
+          </li>
+          <li class="list-inline-item cbp-filter-item g-brd-around g-brd-secondary-light-v2 g-brd-primary--active g-color-gray-dark-v4 g-color-primary--hover g-color-primary--active g-font-size-13 g-rounded-30 g-transition-0_3 g-px-20 g-py-7 mb-2" role="button"
+              data-filter=".design, .identity">Design &amp; Identity
+          </li>
+        </ul>
+        <!-- End Cube Portfolio Blocks - Filter -->
+
+        <div class="cbp g-mb-50"
+             data-controls="#filterControls"
+             data-animation="quicksand"
+             data-x-gap="30"
+             data-y-gap="30"
+             data-media-queries='[
+               {"width": 1500, "cols": 3},
+               {"width": 1100, "cols": 3},
+               {"width": 800, "cols": 3},
+               {"width": 480, "cols": 2},
+               {"width": 300, "cols": 1}
+             ]'>
+
+
+          <!-- Cube Portfolio Blocks - Item -->
+          <div class="cbp-item identity design">
+            <div class="u-block-hover g-parent">
+              <img class="img-fluid g-transform-scale-1_1--parent-hover g-transition-0_5 g-transition--ease-in-out" src="<?php print base_url('assets/images/recipes/1.jpg');?>" alt="Seafood pasta">
+              <div class="d-flex w-100 h-100 g-bg-primary-opacity-0_6 opacity-0 g-opacity-1--parent-hover g-pos-abs g-top-0 g-left-0 g-transition-0_3 g-transition--ease-in u-block-hover__additional--fade u-block-hover__additional--fade-in g-pa-20">
+                <ul class="align-items-end flex-column list-inline mt-auto ml-auto mb-0">
+                  <li class="list-inline-item">
+                    <a class="u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="#!">
+                      <i class="icon-communication-095 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a class="cbp-lightbox u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="<?php print base_url('assets/images/recipes/1.jpg');?>">
+                      <i class="icon-communication-017 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="text-center g-pa-25 mb-1">
+              <h3 class="h5 g-font-weight-600 mb-1">Seafood Pasta</h3>
+              <p class="mb-0">Diana Phelan, Ireland</p>
+            </div>
+          </div>
+          <!-- End Cube Portfolio Blocks - Item -->
+
+          <!-- Cube Portfolio Blocks - Item -->
+          <div class="cbp-item design">
+            <div class="u-block-hover g-parent">
+              <img class="img-fluid g-transform-scale-1_1--parent-hover g-transition-0_5 g-transition--ease-in-out" src="<?php print base_url('assets/images/recipes/2.jpg');?>" alt="Grilled Chicken">
+              <div class="d-flex w-100 h-100 g-bg-primary-opacity-0_6 opacity-0 g-opacity-1--parent-hover g-pos-abs g-top-0 g-left-0 g-transition-0_3 g-transition--ease-in u-block-hover__additional--fade u-block-hover__additional--fade-in g-pa-20">
+                <ul class="align-items-end flex-column list-inline mt-auto ml-auto mb-0">
+                  <li class="list-inline-item">
+                    <a class="u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="#!">
+                      <i class="icon-communication-095 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a class="cbp-lightbox u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="<?php print base_url('assets/images/recipes/1.jpg');?>">
+                      <i class="icon-communication-017 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="text-center g-pa-25 mb-1">
+              <h3 class="h5 g-font-weight-600 mb-1">Grilled Chicken</h3>
+              <p class="mb-0">Lucas Perico, Portugal</p>
+            </div>
+          </div>
+          <!-- End Cube Portfolio Blocks - Item -->
+
+          <!-- Cube Portfolio Blocks - Item -->
+          <div class="cbp-item identity">
+            <div class="u-block-hover g-parent">
+              <img class="img-fluid g-transform-scale-1_1--parent-hover g-transition-0_5 g-transition--ease-in-out" src="<?php print base_url('assets/images/recipes/3.jpg');?>" alt="Grilled Salmon">
+              <div class="d-flex w-100 h-100 g-bg-primary-opacity-0_6 opacity-0 g-opacity-1--parent-hover g-pos-abs g-top-0 g-left-0 g-transition-0_3 g-transition--ease-in u-block-hover__additional--fade u-block-hover__additional--fade-in g-pa-20">
+                <ul class="align-items-end flex-column list-inline mt-auto ml-auto mb-0">
+                  <li class="list-inline-item">
+                    <a class="u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="#!">
+                      <i class="icon-communication-095 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a class="cbp-lightbox u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="<?php print base_url('assets/images/recipes/3.jpg');?>">
+                      <i class="icon-communication-017 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="text-center g-pa-25 mb-1">
+              <h3 class="h5 g-font-weight-600 mb-1">Grilled Salmon</h3>
+              <p class="mb-0">Tamiya June, Ireland</p>
+            </div>
+          </div>
+          <!-- End Cube Portfolio Blocks - Item -->
+
+          <!-- Cube Portfolio Blocks - Item -->
+          <div class="cbp-item design">
+            <div class="u-block-hover g-parent">
+              <img class="img-fluid g-transform-scale-1_1--parent-hover g-transition-0_5 g-transition--ease-in-out" src="<?php print base_url('assets/images/recipes/4.jpg');?>" alt="Beef with Gravy">
+              <div class="d-flex w-100 h-100 g-bg-primary-opacity-0_6 opacity-0 g-opacity-1--parent-hover g-pos-abs g-top-0 g-left-0 g-transition-0_3 g-transition--ease-in u-block-hover__additional--fade u-block-hover__additional--fade-in g-pa-20">
+                <ul class="align-items-end flex-column list-inline mt-auto ml-auto mb-0">
+                  <li class="list-inline-item">
+                    <a class="u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="#!">
+                      <i class="icon-communication-095 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a class="cbp-lightbox u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="<?php print base_url('assets/images/recipes/4.jpg');?>>
+                      <i class="icon-communication-017 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="text-center g-pa-25 mb-1">
+              <h3 class="h5 g-font-weight-600 mb-1">Beef with Gravy</h3>
+              <p class="mb-0">Nyasha Sineti, Zimbabwe</p>
+            </div>
+          </div>
+          <!-- End Cube Portfolio Blocks - Item -->
+
+          <!-- Cube Portfolio Blocks - Item -->
+          <div class="cbp-item identity">
+            <div class="u-block-hover g-parent">
+              <img class="img-fluid g-transform-scale-1_1--parent-hover g-transition-0_5 g-transition--ease-in-out" src="<?php print base_url('assets/images/recipes/5.jpg');?>" alt="Canapes">
+              <div class="d-flex w-100 h-100 g-bg-primary-opacity-0_6 opacity-0 g-opacity-1--parent-hover g-pos-abs g-top-0 g-left-0 g-transition-0_3 g-transition--ease-in u-block-hover__additional--fade u-block-hover__additional--fade-in g-pa-20">
+                <ul class="align-items-end flex-column list-inline mt-auto ml-auto mb-0">
+                  <li class="list-inline-item">
+                    <a class="u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="#!">
+                      <i class="icon-communication-095 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a class="cbp-lightbox u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="<?php print base_url('assets/images/recipes/5.jpg');?>">
+                      <i class="icon-communication-017 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="text-center g-pa-25 mb-1">
+              <h3 class="h5 g-font-weight-600 mb-1">Canapes</h3>
+              <p class="mb-0">Roberto, Itally</p>
+            </div>
+          </div>
+          <!-- End Cube Portfolio Blocks - Item -->
+
+          <!-- Cube Portfolio Blocks - Item -->
+          <div class="cbp-item identity">
+            <div class="u-block-hover g-parent">
+              <img class="img-fluid g-transform-scale-1_1--parent-hover g-transition-0_5 g-transition--ease-in-out" src="<?php print base_url('assets/images/recipes/6.jpg');?>" alt="Finger Food">
+              <div class="d-flex w-100 h-100 g-bg-primary-opacity-0_6 opacity-0 g-opacity-1--parent-hover g-pos-abs g-top-0 g-left-0 g-transition-0_3 g-transition--ease-in u-block-hover__additional--fade u-block-hover__additional--fade-in g-pa-20">
+                <ul class="align-items-end flex-column list-inline mt-auto ml-auto mb-0">
+                  <li class="list-inline-item">
+                    <a class="u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="#!">
+                      <i class="icon-communication-095 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a class="cbp-lightbox u-icon-v2 u-icon-size--sm g-brd-white g-color-black g-bg-white rounded-circle" href="<?php print base_url('assets/images/recipes/6.jpg');?>">
+                      <i class="icon-communication-017 u-line-icon-pro"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="text-center g-pa-25 mb-1">
+              <h3 class="h5 g-font-weight-600 mb-1">Finger Food</h3>
+              <p class="mb-0">Arriana, Itally</p>
+            </div>
+          </div>
+          <!-- End Cube Portfolio Blocks - Item -->
+        </div>
+
+        <div class="text-center">
+          <a class="btn u-btn-primary g-bg-secondary g-color-primary g-color-white--hover g-bg-primary--hover g-font-weight-600 g-font-size-12 g-rounded-30 g-py-15 g-px-35" href="#!">More Recipes</a>
+        </div>
+
+      </div>
