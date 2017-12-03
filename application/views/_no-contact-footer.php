6fd444428eae38f952b68746afbfd254a95a707b
rayphelan <rayphelan@gmail.com>
2017-12-03 09:08:07 +0000

    Update footer links

--- application/views/_no-contact-footer.php
+++ application/views/_no-contact-footer.php
@@ -1,158 +1,107 @@
-  
+    
 <!-- Footer -->
 <footer>
   <div class="g-bg-black">
     <div class="container g-pt-70 g-pb-40">
       <div class="row">
         <div class="col-6 col-md-3 g-mb-30">
+          <h3 class="h6 g-color-white text-uppercase">SiteMap</h3>
+
+          <!-- Links -->
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="<?php print base_url($lg);?>">
+                Home
+                
+              </a>
+            </li>
+            <li>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="<?php print base_url($lg."/categories");?>">
+                Categories
+                
+              </a>
+            </li>
+            <li>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="<?php print base_url($lg."/recipes");?>">
+                Recipes
+                
+              </a>
+            </li>
+
+            <li>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="<?php print base_url($lg."/contact");?>">
+                Contact
+                
+              </a>
+            </li>
+            <li>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="<?php print base_url($lg."/login");?>">
+                Log in
+                
+              </a>
+            </li>
+            <li>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="<?php print base_url($lg."/register");?>">
+                Register
+                
+              </a>
+            </li>            
+            
+          </ul>
+          <!-- End Links -->
+        </div>
+
+        <div class="col-6 col-md-3 g-mb-30">
           <h3 class="h6 g-color-white text-uppercase">About</h3>
+          <p class="g-font-size-13 g-color-gray-dark-v3 g-font-weight-600 g-line-height-2 mb-0">
+            Raymond Phelan <br>
+            Project for Web Design<br>
+            National College of Ireland
+          </p>
+          
+
+        </div>
+
+
+        <div class="col-6 col-md-3 g-mb-30">
+          <h3 class="h6 g-color-white text-uppercase">Social</h3>
 
           <!-- Links -->
           <ul class="list-unstyled mb-0">
             <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="page-about-1.html">
-                About Unify Marketing
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="https://www.facebook.com/ray.phelan" target="_blank">
+                Facebook
+                
               </a>
             </li>
             <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="page-services-1.html">
-                Services
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="https://www.pinterest.pt/rayphelan/">
+                Pinterest
+                
               </a>
             </li>
             <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="page-pricing-1.html">
-                Pricing Tables
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="https://www.instagram.com/rayphelan/">
+                Instagram
+                
               </a>
             </li>
             <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="page-hire-us-1.html">
-                Hire Unify Marketing
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
+              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="https://www.youtube.com/user/rayphelan/videos?view_as=subscriber">
+                Youtube
+                
               </a>
             </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="page-apply-1.html">
-                Apply for a Job
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="page-contacts-1.html">
-                Contacts
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
+                     
+            
           </ul>
           <!-- End Links -->
-        </div>
+        </div>        
 
         <div class="col-6 col-md-3 g-mb-30">
-          <h3 class="h6 g-color-white text-uppercase">Insiders</h3>
-
-          <!-- Links -->
-          <ul class="list-unstyled mb-0">
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Insider Information</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Information for Insiders</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Personal Information</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Enterprise Information</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-          </ul>
-          <!-- End Links -->
-        </div>
-
-        <div class="col-6 col-md-3 g-mb-30">
-          <h3 class="h6 g-color-white text-uppercase">Unify Special</h3>
-
-          <!-- Links -->
-          <ul class="list-unstyled mb-0">
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Unify Online</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Unify Express</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Unify Finance</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Unify Business</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>Unify Ads</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-          </ul>
-          <!-- End Links -->
-        </div>
-
-        <div class="col-6 col-md-3 g-mb-30">
-          <!-- Links -->
-          <ul class="list-inline">
-            <li class="list-inline-item mb-1">
-              <a class="u-icon-v2 u-icon-size--xs g-brd-gray-light-v2 g-brd-primary--hover g-color-gray-light-v1 g-color-primary--hover rounded" href="#">
-                <i class="fa fa-android"></i>
-              </a>
-            </li>
-            <li class="list-inline-item mb-1">
-              <a class="u-icon-v2 u-icon-size--xs g-brd-gray-light-v2 g-brd-primary--hover g-color-gray-light-v1 g-color-primary--hover rounded" href="#">
-                <i class="fa fa-apple"></i>
-              </a>
-            </li>
-            <li class="list-inline-item mb-1">
-              <a class="u-icon-v2 u-icon-size--xs g-brd-gray-light-v2 g-brd-primary--hover g-color-gray-light-v1 g-color-primary--hover rounded" href="#">
-                <i class="fa fa-windows"></i>
-              </a>
-            </li>
-          </ul>
-
-          <ul class="list-unstyled mb-4">
-            <li>
-              <a class="u-link-v6 g-color-gray-dark-v5 g-color-primary--hover g-font-weight-500 g-text-underline--none--hover g-py-3" href="#">
-                <span>How to Install Apps</span>
-                <span class="u-link-v6-arrow g-font-size-18">→</span>
-              </a>
-            </li>
-          </ul>
-          <!-- End Links -->
 
           <h3 class="h6 g-color-white text-uppercase">Address</h3>
-          <address class="g-font-size-13 g-color-gray-dark-v3 g-font-weight-600 g-line-height-2 mb-0">470 Lucy Forks,<br>Patriciafurt, YC7B 3UT<br>WY10037, Great Britain</address>
+          <address class="g-font-size-13 g-color-gray-dark-v3 g-font-weight-600 g-line-height-2 mb-0">Newtownforbes,<br>Co. Longford<br>Ireland</address>
         </div>
       </div>
     </div>
@@ -164,10 +113,10 @@
         <div class="col-md-4 text-center text-md-left g-mb-30">
           <!-- Logo -->
           <a class="g-text-underline--none--hover mr-4" href="index.html">
-            <img class="g-width-95" src="../../../assets/img/logo/logo-2.png" alt="Logo">
+            <img class="g-width-95" src="<?php print base_url('assets/images/logo/logo.png');?>" alt="Cooking Good">
           </a>
           <!-- End Logo -->
-          <p class="d-inline-block align-middle g-color-gray-dark-v5 g-font-size-13 mb-0">© 2017 Htmlstream.
+          <p class="d-inline-block align-middle g-color-gray-dark-v5 g-font-size-13 mb-0">© 2017 Raymond Phelan.
             <br>All Rights Reserved.</p>
         </div>
 
@@ -175,19 +124,16 @@
           <!-- Social Icons -->
           <ul class="list-inline text-center mb-0">
             <li class="list-inline-item">
-              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="#"><i class="fa fa-facebook"></i></a>
+              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="https://www.facebook.com/ray.phelan" target="_blank"><i class="fa fa-facebook"></i></a>
             </li>
             <li class="list-inline-item">
-              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="#"><i class="fa fa-twitter"></i></a>
+              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="https://www.pinterest.pt/rayphelan/" target="_blank"><i class="fa fa-pinterest"></i></a>
             </li>
             <li class="list-inline-item">
-              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="#"><i class="fa fa-pinterest"></i></a>
+              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="https://www.instagram.com/rayphelan/" target="_blank"><i class="fa fa-instagram"></i></a>
             </li>
             <li class="list-inline-item">
-              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="#"><i class="fa fa-instagram"></i></a>
-            </li>
-            <li class="list-inline-item">
-              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="#"><i class="fa fa-youtube"></i></a>
+              <a class="u-icon-v3 u-icon-size--sm g-color-gray-light-v1 g-color-white--hover g-bg-transparent g-bg-primary--hover rounded" href="https://www.youtube.com/user/rayphelan/videos?view_as=subscriber" target="_blank"><i class="fa fa-youtube"></i></a>
             </li>
           </ul>
           <!-- End Social Icons -->
@@ -196,12 +142,9 @@
         <div class="col-md-4 g-mb-30 text-right">
           <div class="d-inline-block g-mx-15">
             <h4 class="g-color-gray-dark-v5 g-font-size-11 text-left text-uppercase">Email</h4>
-            <a href="#">unify@marketing.com</a>
+            <a href="#">x17119847@student.ncirl.ie</a>
           </div>
-          <div class="d-inline-block g-mx-15">
-            <h4 class="g-color-gray-dark-v5 g-font-size-11 text-left text-uppercase">Phone</h4>
-            <a href="#">(0161) 347 8854</a>
-          </div>
+          
         </div>
       </div>
     </div>
